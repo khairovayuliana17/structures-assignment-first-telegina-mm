758d8fc06217f3c01ed0a800de3bfd80730325d6
telegina-mm <teleginamaria988@gmail.com>
2025-07-20 10:09:38 +0300

    my solution

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,5 +1,17 @@
 #include "circle.h"
 
 //  your code here (functions)
+#include <cmath>
 
+double circleArea(Circle c) { return M_PI * c.radius * c.radius; }
 
+double circleLength(Circle c) { return 2 * M_PI * c.radius; }
+
+bool isPointInside(Circle c, Point p) {
+    double dx = p.x - c.center.x;
+    double dy = p.y - c.center.y;
+    double distance = dx * dx + dy * dy;
+    double radius = c.radius * c.radius;
+
+    return distance <= radius;
+}
